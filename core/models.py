c543c678d49da44a21beb93a1f8e5126f61c6f8b
DasCavernas <109463115+DasCavernas@users.noreply.github.com>
2022-07-18 21:17:29 -0300

    Criado tela de listagem de eventos.

--- core/models.py
+++ core/models.py
@@ -1,18 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
-
 # Create your models here.
+from django.db.models import TextField
 
 
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
+    local = models.TextField(blank=False)
     descricao = models.TextField(blank=True, null=True)
     date_evento = models.DateTimeField(verbose_name='Data do evento')
     data_criacao = models.DateTimeField(auto_now=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
+
     class Meta:
         db_table = 'evento'
 
     def __str__(self):
-        return self.titulo
+        return self.local
+
+    def get_data_evento(self):
+        return self.date_evento.strftime('%d/%m/%Y %H:%M Horas')
+
+
+
--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from core.models import Evento
 
-# Create your views here.
+# Create your views here
+
+def eventos(request, nome_evento):
+    local = Evento.objects.get(titulo=nome_evento)
+    return HttpResponse(f'{local}')
+
+#def index(request):
+    #return redirect('/agenda/')
+
+
+def lista_eventos(request):
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)
+    dados = {'eventos':evento}
+    return render(request, 'agenda.html', dados)
+
